bf5cfccae09d85cd55cb501f8c2ce23f670a476b
Abhishek Kumar <abhisheksmscollege@gmail.com>
2023-10-17 16:20:15 +0530

    first commit

--- main.tf
+++ main.tf
@@ -1,11 +1,29 @@
-provider "aws" {
-    region = "us-east-1"  
+# Terraform Block
+terraform {
+  required_version = ">= 1.4" 
+  required_providers {
+    aws = {
+      version = "~> 3.0"
+    }
+  }
 }
 
-resource "aws_instance" "foo" {
-  ami           = "ami-05fa00d4c63e32376" # us-west-2
+
+# Provider Block
+provider "aws" {
+  region  = "us-east-1"
+  profile = "default"
+}
+/*
+Note-1:  AWS Credentials Profile (profile = "default") configured on your local desktop terminal  
+$HOME/.aws/credentials
+*/
+# Create EC2 Instance
+resource "aws_instance" "web" {
+  ami           = "ami-053b0d53c279acc90" # Amazon Linux
   instance_type = "t2.micro"
+   key_name     = "ec2-create"
   tags = {
-      Name = "TF-Instance"
+    "Name" = "web"
   }
 }
